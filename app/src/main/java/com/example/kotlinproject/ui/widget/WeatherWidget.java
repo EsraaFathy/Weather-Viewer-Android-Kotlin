ece7a9aeb6a8e9fd6b6a996329835d87d68ccc68
Esraa Fathy <fesraa240@gmail.com>
2021-03-16 04:27:38 +0200

    update ui in widget

--- app/src/main/java/com/example/kotlinproject/ui/widget/WeatherWidget.java
+++ app/src/main/java/com/example/kotlinproject/ui/widget/WeatherWidget.java
@@ -1,10 +1,15 @@
 package com.example.kotlinproject.ui.widget;
 
 import android.app.Application;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.AppWidgetTarget;
@@ -19,13 +24,17 @@ import com.example.kotlinproject.ui.GeneralFunctions;
  * Implementation of App Widget functionality.
  */
 public class WeatherWidget extends AppWidgetProvider {
-
+    private static final String MY_BUTTTON_START = "myButtonStart";
+    private static RemoteViews views;
+    private static AppWidgetManager appWidgetManage;
+    private static int appWidgtId;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
+       appWidgetManage=appWidgetManager;
+        appWidgtId=appWidgetId;
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
+        views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
 //        views.setTextViewText(R.id.appwidget_text, widgetText);
         RoomRepositry roomRepositry=new RoomRepositry((Application) context.getApplicationContext());
         GeneralFunctions generalFunctions=new GeneralFunctions();
@@ -34,8 +43,6 @@ public class WeatherWidget extends AppWidgetProvider {
         views.setTextViewText(R.id.currentTemp,""+current.getCurrent().getTemp());
         views.setTextViewText(R.id.currentTime,generalFunctions.formateTime(current.getCurrent().getDt()));
         views.setTextViewText(R.id.currentDate,generalFunctions.formateDate(current.getCurrent().getDt()));
-//        generalFunctions.loadImage();
-
         AppWidgetTarget appWidgetTarget = new AppWidgetTarget( context , R.id.currentModeImg, views,appWidgetId );
 
         Glide.with(context.getApplicationContext())
@@ -43,6 +50,11 @@ public class WeatherWidget extends AppWidgetProvider {
                 .load("https://openweathermap.org/img/w/"+current.getCurrent().getWeather().get(0).getIcon()+".png")
                 .into(appWidgetTarget);
         // Instruct the widget manager to update the widget
+        Intent intent = new Intent(context, WeatherWidget.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.currentModeImg, getPendingSelfIntent(context, MY_BUTTTON_START));
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -62,5 +74,36 @@ public class WeatherWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (MY_BUTTTON_START.equals(intent.getAction())){
+//            Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
+            views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
+            RoomRepositry roomRepositry=new RoomRepositry((Application) context.getApplicationContext());
+            GeneralFunctions generalFunctions=new GeneralFunctions();
+            AllData current=roomRepositry.getData().get(0);
+            views.setTextViewText(R.id.currentCity,current.getTimezone());
+            views.setTextViewText(R.id.currentTemp,""+current.getCurrent().getTemp());
+            views.setTextViewText(R.id.currentTime,generalFunctions.formateTime(current.getCurrent().getDt()));
+            views.setTextViewText(R.id.currentDate,generalFunctions.formateDate(current.getCurrent().getDt()));
+            AppWidgetTarget appWidgetTarget = new AppWidgetTarget( context , R.id.currentModeImg, views,appWidgtId );
+
+            Glide.with(context.getApplicationContext())
+                    .asBitmap()
+                    .load("https://openweathermap.org/img/w/"+current.getCurrent().getWeather().get(0).getIcon()+".png")
+                    .into(appWidgetTarget);
+
+            appWidgetManage.updateAppWidget(appWidgtId, views);
+
+        }
+    }
+
+    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, WeatherWidget.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
