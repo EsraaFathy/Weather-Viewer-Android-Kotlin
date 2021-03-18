96ab2ead501c9e12643e6e069b672ab0b3672e6e
Esraa Fathy <fesraa240@gmail.com>
2021-03-18 18:42:45 +0200

    final widget

--- app/src/main/java/com/example/kotlinproject/ui/baseHome/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproject/ui/baseHome/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.kotlinproject.ui.baseHome
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
